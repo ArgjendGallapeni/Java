ea1aa7e4f793cdb0f0b6ed85c0bcf9a2bad0df87
KastriotBlakaj <lotiblakaj123@gmail.com>
2024-01-23 08:49:03 +0100

    numrat qift dhe tek

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 package src;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        numratQiftDheTek();
+    }
+
+    public static void numratQiftDheTek(){
+        Random random = new Random();
+        int x = random.nextInt(100);
+        if(x%2==0){
+            System.out.println("Numri eshte qift :"+x);
+        }else {
+            System.out.println("Numri eshte tek ! ");
+        }
     }
 }
