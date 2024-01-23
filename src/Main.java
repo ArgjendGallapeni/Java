4d7dfcea882e37f5f1ce2f097c2d636296d4f0dd
ArgjendGallapeni <gallapenia9@gmail.com>
2024-01-23 09:17:36 +0100

    homewok
    homework

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,33 @@ package src;
 
 import java.util.Random;
 
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
         numratQiftDheTek();
+        Numrat();
     }
 
-    public static void numratQiftDheTek(){
+
+        public static void numratQiftDheTek(){
+            Random random = new Random();
+            int x = random.nextInt(100);
+            if(x%2==0){
+                System.out.println("Numri eshte qift :"+x);
+            }else {
+                System.out.println("Numri eshte tek ! ");
+            }
+        }
+
+    public static void Numrat (){
         Random random = new Random();
         int x = random.nextInt(100);
-        if(x%2==0){
-            System.out.println("Numri eshte qift :"+x);
+        if (x%5==0) {
+            System.out.println("Numri esht qift");
         }else {
-            System.out.println("Numri eshte tek ! ");
+            System.out.println("Numri esht tek");
+        }
         }
     }
-    
-}
