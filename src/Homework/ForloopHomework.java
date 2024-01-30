b80d278d243d052229d2462fd93993ebffac1689
ArgjendGallapeni <gallapenia9@gmail.com>
2024-01-30 10:28:56 +0100

    faktoriel

--- src/Homework/ForloopHomework.java
+++ src/Homework/ForloopHomework.java
@@ -6,12 +6,14 @@ public class ForloopHomework {
         //Itero nga 0-100 dhe gjej shumen e numrave
         int shuma = 0;
         for (int i = 0; i <= 10; i++) {
-            shuma = shuma+i;
+            shuma = shuma + i;
         }
-        System.out.println("Shuma numrave nga 1-100 eshte :" +shuma);
+        System.out.println("Shuma numrave nga 1-100 eshte :" + shuma);
         numrat();
         mesatarja();
+        faktoriel();
     }
+
     public static void numrat() {
         //itero nga 1-100 gjej numrat me tmdhej se 50 dhe te pjotepjeshtuem me 6
         for (int i = 1; i <= 100; i++) {
@@ -21,12 +23,20 @@ public class ForloopHomework {
         }
     }
 
-    public static void mesatarja(){
+    public static void mesatarja() {
         //Gjej mesatrene nga shuma e iterimit te numrave nga 1-100
         int shuma = 0;
         for (int i = 0; i <= 100; i++) {
-            shuma = shuma+i;
+            shuma = shuma + i;
         }
-        System.out.println("Mesatarja : "+shuma/100);
+        System.out.println("Mesatarja : " + shuma / 100);
+    }
+
+    public static void faktoriel() {
+        int prodhimi = 1;
+        for (int i = 1; i <= 5; i++) {
+            prodhimi = prodhimi * i;
+        }
+        System.out.println(prodhimi);
     }
 }
