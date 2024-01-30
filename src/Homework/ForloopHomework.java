dd6baab7a06b0dce655cecab7a18e4b8c716b148
KastriotBlakaj <lotiblakaj123@gmail.com>
2024-01-30 10:11:11 +0100

    review and a push

--- src/Homework/ForloopHomework.java
+++ src/Homework/ForloopHomework.java
@@ -3,19 +3,30 @@ package src.Homework;
 public class ForloopHomework {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println("Numri : "+i);
-
-
-
+        //Itero nga 0-100 dhe gjej shumen e numrave
+        int shuma = 0;
+        for (int i = 0; i <= 10; i++) {
+            shuma = shuma+i;
         }
-
-        public static void numrat(){
-            for (int i = 1; i <= 100; i++) {
-                if (i > 50 && i % 6 == 0) {
-                    System.out.println("Numri : " + i);
-                }
-
+        System.out.println("Shuma numrave nga 1-100 eshte :" +shuma);
+        numrat();
+        mesatarja();
+    }
+    public static void numrat() {
+        //itero nga 1-100 gjej numrat me tmdhej se 50 dhe te pjotepjeshtuem me 6
+        for (int i = 1; i <= 100; i++) {
+            if (i > 50 && i % 6 == 0) {
+                System.out.println("Numri : " + i);
             }
-            }
+        }
+    }
+
+    public static void mesatarja(){
+        //Gjej mesatrene nga shuma e iterimit te numrave nga 1-100
+        int shuma = 0;
+        for (int i = 0; i <= 100; i++) {
+            shuma = shuma+i;
+        }
+        System.out.println("Mesatarja : "+shuma/100);
+    }
 }
